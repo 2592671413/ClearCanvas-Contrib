9a8245340fa8e5e9ae07fb1fb656ea9a39f96751
econmed GmbH <econmed@users.noreply.github.com>
2016-11-27 23:46:27 +0100

    bugfix and improvements for the install.bat

--- ExplodeImageBoxesTool/install.bat
+++ ExplodeImageBoxesTool/install.bat
@@ -2,11 +2,14 @@
 cls
 setlocal
 
+set resGenPath=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\ResGen.exe
 set installationPath=%ProgramFiles%\ClearCanvas\ClearCanvas DICOM Viewer
 set /p installationPath=Enter the ClearCanvas Workstation or DicomViewer installation directory [%installationPath%]: 
 echo.
 pushd %installationPath%
-"%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\Bin\ResGen.exe" "%~dp0\source\SR.resx"
+if exist "%resGenPath%" (
+  "%resGenPath%" "%~dp0\source\SR.resx"
+)
 "%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\csc.exe" /nologo ^
 /target:library ^
 /reference:.\common\ClearCanvas.Common.dll ^
@@ -14,14 +17,11 @@ pushd %installationPath%
 /reference:.\plugins\ClearCanvas.Desktop.dll ^
 /reference:.\plugins\ClearCanvas.ImageViewer.dll ^
 /out:.\plugins\Econmed.ImageViewer.Layout.ExplodeImageBoxesTool.dll ^
-/resource:%~dp0\source\SR.resources,Econmed.ImageViewer.Layout.SR.resources ^
-/resource:%~dp0\source\Icons\ExplodeImageBoxLarge.png,Econmed.ImageViewer.Layout.Icons.ExplodeImageBoxLarge.png ^
-/resource:%~dp0\source\Icons\ExplodeImageBoxMedium.png,Econmed.ImageViewer.Layout.Icons.ExplodeImageBoxMedium.png ^
-/resource:%~dp0\source\Icons\ExplodeImageBoxToolSmall.png,Econmed.ImageViewer.Layout.Icons.ExplodeImageBoxToolSmall.png ^
-%~dp0\source\ExplodeImageBoxesTool.cs ^
-%~dp0\source\ListObserver.cs ^
-%~dp0\source\Properties\AssemblyInfo.cs ^
-%~dp0\source\SR.Designer.cs
+/resource:"%~dp0\source\SR.resources",Econmed.ImageViewer.Layout.SR.resources ^
+/resource:"%~dp0\source\Icons\ExplodeImageBoxLarge.png",Econmed.ImageViewer.Layout.Icons.ExplodeImageBoxLarge.png ^
+/resource:"%~dp0\source\Icons\ExplodeImageBoxMedium.png",Econmed.ImageViewer.Layout.Icons.ExplodeImageBoxMedium.png ^
+/resource:"%~dp0\source\Icons\ExplodeImageBoxToolSmall.png",Econmed.ImageViewer.Layout.Icons.ExplodeImageBoxToolSmall.png ^
+"%~dp0\source\*.cs"
 popd
 echo.
 if errorlevel 1 (
